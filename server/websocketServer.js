0239254a49a0b54fbec3b7f5529747f098cf0f29
Narendra Sisodiya <narendra@billexo.com>
2017-05-31 20:19:47 +0530

    When we Hit on some GET URL, data to be triggered on WebSocket.

--- server/websocketServer.js
+++ server/websocketServer.js
@@ -2,22 +2,35 @@
  * Created by narendrasisodiya on 31/05/17.
  */
 
+import EventBus from "@nsisodiya/eventbus";
+
+var evtBus = new EventBus();
+
 export default function websocketServer(app) {
   require("express-ws")(app);
+
+  var commonDb = {
+    count: 100
+  };
+
+  app.get("/jump", function(req, res) {
+    res.send("Jumping Success");
+    commonDb.count = commonDb.count + 100;
+    evtBus.publish("UPDATE_AVAILABLE");
+  });
   app.ws("/", function(ws /*, req*/) {
     console.log("Client initiated Connection Request");
     ws.on("message", function(msg) {
       console.log("Got Message from Client", msg);
     });
-    let count = 100;
-    var id = setInterval(function() {
-      count = count + 1;
-      ws.send(JSON.stringify({ count }));
-    }, 1000);
+
+    var unsubFunc = evtBus.subscribe("UPDATE_AVAILABLE", function() {
+      ws.send(JSON.stringify({ count: commonDb.count }));
+    });
+
     ws.on("close", function() {
+      unsubFunc();
       console.log("Stopping client interval");
-      clearInterval(id);
     });
   });
-
 }
